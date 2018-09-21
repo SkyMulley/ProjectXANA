4df004d9f805a2bda326279a9eba9b9a3cca585e
SkySmith2 <daniel.mulley@Outlook.com>
2018-09-21 23:43:44 +0100

    Fixed up LogicCore

--- src/pro/mrcl/ProjectXANA/AttackLogic/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/AttackLogic/LogicCore.java
@@ -1,13 +1,14 @@
 package pro.mrcl.ProjectXANA.AttackLogic;
 
-import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
 import pro.mrcl.ProjectXANA.AttackLogic.Easy.LaughingGas;
+import pro.mrcl.ProjectXANA.AttackLogic.Pathetic.XanafiedJim;
 
 
-public class ProjectXANAModule extends AttackModule {
-    public ProjectXANAModule() {
+public class LogicCore extends AttackModule {
+    public LogicCore() {
         super("ProjectXANACore","Sky");
         addAttack(new LaughingGas());
+        addAttack(new XanafiedJim());
     }
 }
