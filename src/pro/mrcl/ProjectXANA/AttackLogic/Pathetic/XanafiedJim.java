43745c714c2ed7817e8b000e88227b6c78e69522
SkySmith2 <daniel.mulley@Outlook.com>
2018-09-21 23:43:34 +0100

    Created XanafiedJim base

---
+++ src/pro/mrcl/ProjectXANA/AttackLogic/Pathetic/XanafiedJim.java
@@ -0,0 +1,41 @@
+package pro.mrcl.ProjectXANA.AttackLogic.Pathetic;
+
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Events.AttackEvents.AttackEndEvent;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AbstractAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.Core.Pathethic.SimpleActivationAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Difficulty.ATTACKDIFFICULTY;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.VirtualStructures.Tower;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+
+public class XanafiedJim extends AbstractAttack {
+    private Tower tower = null;
+    private SimpleActivationAttack towerAttack;
+    private Main pl;
+    public XanafiedJim() {
+        super(ATTACKDIFFICULTY.EASY);
+    }
+
+    @Override
+    public boolean startAttack() {
+        try {
+            towerAttack = new SimpleActivationAttack();
+            towerAttack.startAttack();
+            registerListener(new org.bukkit.event.Listener() {
+                @EventHandler
+                public void onAttackEnd(AttackEndEvent AEE) {
+                    if (AEE.getAttack().equals(towerAttack)) {
+                        stopAttack();
+                        //removeblockshere
+                    }
+                }
+            });
+            //setblockshere
+            return true;
+        } catch (Exception e) {
+            Bukkit.getLogger().info("[PRX] Something went wrong while running a XanafiedJim attack: " + e);
+            return true;
+        }
+    }
+}
