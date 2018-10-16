d09d89155fc06fc4dbaee68d8384a10735494e36
Daniel Mulley <daniel.mulley@outlook.com>
2018-10-16 12:22:07 +0100

    Misc

--- src/pro/mrcl/ProjectXANA/AttackLogic/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/AttackLogic/LogicCore.java
@@ -3,7 +3,6 @@ package pro.mrcl.ProjectXANA.AttackLogic;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
 import pro.mrcl.ProjectXANA.AttackLogic.Easy.LaughingGas;
 import pro.mrcl.ProjectXANA.AttackLogic.Medium.NuclearPlant;
-import pro.mrcl.ProjectXANA.AttackLogic.Medium.ZeroGravity;
 
 
 public class LogicCore extends AttackModule {
