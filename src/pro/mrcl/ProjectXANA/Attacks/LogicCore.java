7eced8fcf61743912cc62d08e6cdc6a2954959ec
SkySmith2 <daniel.mulley@Outlook.com>
2019-04-15 17:12:56 +0100

    Forgot to actually add the attacks to the module

--- src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
@@ -1,9 +1,13 @@
 package pro.mrcl.ProjectXANA.Attacks;
 
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
+import pro.mrcl.ProjectXANA.Attacks.Medium.RTTPAttack;
+import pro.mrcl.ProjectXANA.Attacks.Pathetic.ElevatorLockAttack;
 
 public class LogicCore extends AttackModule {
     public LogicCore() {
         super("PRXCore","Sky");
+        addAttack(new ElevatorLockAttack());
+        addAttack(new RTTPAttack());
     }
 }
