3d85f9572beccdf3c23ca0157d27d9d05c2099f9
SkySmith2 <daniel.mulley@Outlook.com>
2019-04-15 17:01:24 +0100

    Added tested working attacks

---
+++ src/pro/mrcl/ProjectXANA/Attacks/Medium/RTTPAttack.java
@@ -0,0 +1,57 @@
+package pro.mrcl.ProjectXANA.Attacks.Medium;
+
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Events.AttackEvents.AttackEndEvent;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AbstractAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.Core.Pathethic.SimpleActivationAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Difficulty.ATTACKDIFFICULTY;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
+import org.bukkit.event.EventHandler;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class RTTPAttack extends AbstractAttack {
+    private SimpleActivationAttack towerAttack;
+    private int attackID;
+    public RTTPAttack() { super(ATTACKDIFFICULTY.MEDIUM);}
+
+    @Override
+    public boolean startAttack() {
+        boolean wasActivated = isActivated();
+        super.startAttack();
+        if (!wasActivated) {
+            towerAttack = new SimpleActivationAttack();
+            towerAttack.startAttack();
+            registerListener(new org.bukkit.event.Listener() {
+                @EventHandler
+                public void onAttackEnd(AttackEndEvent e) {
+                    if (e.getAttack().equals(towerAttack)) {
+                        safeStopAttack();
+                    }
+                }
+            });
+            BukkitScheduler scheduler = getServer().getScheduler();
+            attackID = scheduler.scheduleSyncRepeatingTask(Main.getMainInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Main.getMainInstance().getNetwork().getReturnToThePast().activate(true);
+                        Bukkit.broadcastMessage("The Return to the Past program seems to be bugging out, what could be the issue?");
+                    } catch (Exception e) {
+                        safeStopAttack();
+                    }
+                }
+            }, 0L, 12000L);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean stopAttack() {
+        super.stopAttack();
+        Bukkit.getScheduler().cancelTask(attackID);
+        return true;
+    }
+}
---
+++ src/pro/mrcl/ProjectXANA/Attacks/Pathetic/ElevatorLockAttack.java
@@ -0,0 +1,46 @@
+package pro.mrcl.ProjectXANA.Attacks.Pathetic;
+
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Events.AttackEvents.AttackEndEvent;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AbstractAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.Core.Pathethic.SimpleActivationAttack;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Difficulty.ATTACKDIFFICULTY;
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Main;
+import net.minecraft.server.v1_13_R2.CommandSay;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class ElevatorLockAttack extends AbstractAttack {
+    private SimpleActivationAttack towerAttack;
+    public ElevatorLockAttack() { super(ATTACKDIFFICULTY.PATHEHTIC);}
+
+    @Override
+    public boolean startAttack() {
+        boolean wasActivated = isActivated();
+        super.startAttack();
+        if (!wasActivated) {
+            towerAttack = new SimpleActivationAttack();
+            towerAttack.startAttack();
+            registerListener(new org.bukkit.event.Listener() {
+                @EventHandler
+                public void onAttackEnd(AttackEndEvent e) {
+                    if (e.getAttack().equals(towerAttack)) {
+                        safeStopAttack();
+                    }
+                }
+            });
+            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),"elevator edit factory disabled yes");
+        }
+        return false;
+    }
+
+    @Override
+    public boolean stopAttack() {
+        super.stopAttack();
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),"elevator edit factory disabled no");
+        return true;
+    }
+}
