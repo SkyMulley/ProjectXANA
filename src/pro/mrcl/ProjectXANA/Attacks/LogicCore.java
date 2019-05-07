5cd4c8951d0909350e0fe61c616f8b66d55a65a3
SkySmith2 <daniel.mulley@Outlook.com>
2019-05-07 14:19:56 +0100

    Added Group Xanafication attack

--- src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
@@ -1,13 +1,16 @@
 package pro.mrcl.ProjectXANA.Attacks;
 
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
+import pro.mrcl.ProjectXANA.Attacks.Hard.GroupXanafication;
+import pro.mrcl.ProjectXANA.Attacks.Medium.NuclearPlant;
 import pro.mrcl.ProjectXANA.Attacks.Medium.RTTPAttack;
 import pro.mrcl.ProjectXANA.Attacks.Pathetic.ElevatorLockAttack;
 
 public class LogicCore extends AttackModule {
     public LogicCore() {
         super("PRXCore","Sky");
-        addAttack(new ElevatorLockAttack());
         addAttack(new RTTPAttack());
+        addAttack(new NuclearPlant());
+        addAttack(new GroupXanafication());
     }
 }
