99ffcd252487d16fd5521e73ed09d009b8aa4d74
skysmith2 <daniel.mulley@outlook.com>
2019-05-31 13:28:26 +0100

    Add NuclearPlant attack to the roster (Ready for PRX V1.1B)

--- src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
@@ -1,6 +1,7 @@
 package pro.mrcl.ProjectXANA.Attacks;
 
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
+import pro.mrcl.ProjectXANA.Attacks.Medium.NuclearPlant;
 import pro.mrcl.ProjectXANA.Attacks.Medium.RTTPAttack;
 import pro.mrcl.ProjectXANA.Attacks.Pathetic.ElevatorLockAttack;
 
@@ -9,5 +10,6 @@ public class LogicCore extends AttackModule {
         super("PRXCore","Sky");
         addAttack(new ElevatorLockAttack());
         addAttack(new RTTPAttack());
+        addAttack(new NuclearPlant());
     }
 }
