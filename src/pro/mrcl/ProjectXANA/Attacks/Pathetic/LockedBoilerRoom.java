261ebaa54c2cd79cbf809e0a90fb76e3eb6ca459
skymulley <danielmulley2002@gmail.com>
2019-08-07 00:27:24 +0100

    Fixed issue where LockedBoilerRoom attack wouldn't actually do anything

--- src/pro/mrcl/ProjectXANA/Attacks/Pathetic/LockedBoilerRoom.java
+++ src/pro/mrcl/ProjectXANA/Attacks/Pathetic/LockedBoilerRoom.java
@@ -1,14 +1,11 @@
 package pro.mrcl.ProjectXANA.Attacks.Pathetic;
 
-import com.mysql.jdbc.Buffer;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Events.AttackEvents.AttackEndEvent;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AbstractAttack;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.Core.Pathethic.SimpleActivationAttack;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Difficulty.ATTACKDIFFICULTY;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.type.Door;
@@ -18,7 +15,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class LockedBoilerRoom extends AbstractAttack {
     private SimpleActivationAttack towerAttack;
-    private Block door = Bukkit.getWorld("NewKadic").getBlockAt(-125,103,153);
+    private Block doorBlock = Bukkit.getWorld("NewKadic").getBlockAt(-125,103,153);
     public LockedBoilerRoom() { super(ATTACKDIFFICULTY.PATHEHTIC);}
 
     @Override
@@ -28,9 +25,9 @@ public class LockedBoilerRoom extends AbstractAttack {
         if (!wasActivated) {
             towerAttack = new SimpleActivationAttack();
             towerAttack.startAttack();
-            BlockState state = door.getState();
-            Door door = (Door)state.getData();
+            Door door = (Door) doorBlock.getBlockData();
             door.setOpen(false);
+            doorBlock.setBlockData(door);
             registerListener(new org.bukkit.event.Listener() {
                 @EventHandler
                 public void onAttackEnd(AttackEndEvent e) {
@@ -42,8 +39,8 @@ public class LockedBoilerRoom extends AbstractAttack {
             registerListener(new org.bukkit.event.Listener() {
                 @EventHandler
                 public void onInteract(PlayerInteractEvent event) {
-                    if(event.hasBlock()) {
-                        if(event.getClickedBlock().equals(door)) {
+                    if(event.getClickedBlock()!=null) {
+                        if(event.getClickedBlock().getLocation().equals(doorBlock.getLocation().clone()) || event.getClickedBlock().getLocation().equals(doorBlock.getLocation().clone().add(0,1,0))) {
                             if(event.getAction()== Action.RIGHT_CLICK_BLOCK) {
                                 event.getPlayer().sendMessage(ChatColor.RED+"The door is locked! You'll have to find another way around...");
                                 event.setCancelled(true);
