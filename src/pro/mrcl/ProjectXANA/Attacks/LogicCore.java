8a5fd677a25d730792c9b0ef96a514cc879512e9
skymulley <danielmulley2002@gmail.com>
2019-08-07 12:18:59 +0100

    Change BadWeatherAttack from a Pathetic to Easy difficulty

--- src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
@@ -1,6 +1,7 @@
 package pro.mrcl.ProjectXANA.Attacks;
 
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
+import pro.mrcl.ProjectXANA.Attacks.Easy.BadWeatherAttack;
 import pro.mrcl.ProjectXANA.Attacks.Hard.GroupXanafication;
 import pro.mrcl.ProjectXANA.Attacks.Medium.NuclearPlant;
 import pro.mrcl.ProjectXANA.Attacks.Medium.RTTPAttack;
