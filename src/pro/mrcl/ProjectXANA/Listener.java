a36b91192ac6fcbf59ef68bb29d64ab773be3ad9
skymulley <danielmulley2002@gmail.com>
2019-08-08 11:44:22 +0100

    Removed old listeners

--- src/pro/mrcl/ProjectXANA/Listener.java
+++ src/pro/mrcl/ProjectXANA/Listener.java
@@ -1,13 +1,9 @@
 package pro.mrcl.ProjectXANA;
 
+import mrcl.pro.GoodOldJack12.ProjectCarthage.Main;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class Listener implements org.bukkit.event.Listener {
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        if(event.getDeathMessage().equalsIgnoreCase("fell out of the world")) {
-            event.setDeathMessage("was killed in a XANA attack");
-        }
-    }
+    //Remove listeners
 }
