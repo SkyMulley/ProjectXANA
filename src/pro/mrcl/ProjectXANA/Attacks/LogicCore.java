79877ffdc2ef97205ec484efca026bca875f14bb
skymulley <danielmulley2002@gmail.com>
2019-08-08 11:44:11 +0100

    Added new attacks to logic core

--- src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
+++ src/pro/mrcl/ProjectXANA/Attacks/LogicCore.java
@@ -3,6 +3,8 @@ package pro.mrcl.ProjectXANA.Attacks;
 import mrcl.pro.GoodOldJack12.ProjectCarthage.Logic.Programs.Xana.Attacks.AttackModule;
 import pro.mrcl.ProjectXANA.Attacks.Easy.BadWeatherAttack;
 import pro.mrcl.ProjectXANA.Attacks.Hard.GroupXanafication;
+import pro.mrcl.ProjectXANA.Attacks.Hard.Meteorite;
+import pro.mrcl.ProjectXANA.Attacks.Medium.ChemicalTrain;
 import pro.mrcl.ProjectXANA.Attacks.Medium.NuclearPlant;
 import pro.mrcl.ProjectXANA.Attacks.Medium.RTTPAttack;
 import pro.mrcl.ProjectXANA.Attacks.Pathetic.*;
@@ -18,5 +20,7 @@ public class LogicCore extends AttackModule {
         addAttack(new BadWeatherAttack());
         addAttack(new LockedBoilerRoom());
         addAttack(new ElevatorLockAttack());
+        addAttack(new Meteorite());
+        addAttack(new ChemicalTrain());
     }
 }
